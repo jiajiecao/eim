ddceec08b2ffa808d42c7bb1551cba5773830638
York Chen <qgmzwrym@gmail.com>
2019-01-04 12:46:26 +0800

    fix bugs.

--- src/scripts/libs/oex/jqm-email.js
+++ src/scripts/libs/oex/jqm-email.js
@@ -1,14 +1,16 @@
 // note: this does not rely on knockout's number binding
-ko.bindingHandlers[getBindingName("email")] = (function() {
+ko.bindingHandlers[getBindingName("email")] = (function () {
     return {
-        init: function(element, valueAccessor, allBindings) {
+        init: function (element, valueAccessor, allBindings) {
             var modelValue = valueAccessor();
             var $element = $(element);
-            var handler = function(e) {
+            var handler = function (e) {
                 var text = $element.val().trim();
                 var reg = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
                 var isValid = reg.test(text);
-                console.log(isValid);
+                if (!isValid) {
+                    text = "";
+                }
                 $element.val(text);
                 modelValue(text);
             };
@@ -16,11 +18,11 @@ ko.bindingHandlers[getBindingName("email")] = (function() {
             $element.on("change", handler);
         },
 
-        update: function(element, valueAccessor, allBindings) {
+        update: function (element, valueAccessor, allBindings) {
             var value = ko.unwrap(valueAccessor());
             var $element = $(element);
 
-            if (value == null || typeof(value) == "undefined" || typeof(value) == "string" && value.trim() == "") {
+            if (value == null || typeof (value) == "undefined" || typeof (value) == "string" && value.trim() == "") {
                 value = "";
             }
 
